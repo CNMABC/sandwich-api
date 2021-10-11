a90a86135f00b46d1841ae7728dee7b4c70f6df8
Cecilia Nunn <cecilia.nunn@gmail.com>
2021-10-11 11:26:27 +0100

    first setup

--- models.js/schedule.js
+++ models.js/schedule.js
@@ -1,9 +1,9 @@
-import mongoose from 'mongoose'
+// import mongoose from 'mongoose'
 
-// define the schema 
-const scheduleSchema = new mongoose.Schema({
-  sequenceNumber: { type: Number, required: true, unique: true },
-  timestamps: true ,
-  task: { type: String, required: true, maxlength : 20 },
-  recipient: { type: String, required: true, maxlength: 40 },
-})
+// // define the schema 
+// const scheduleSchema = new mongoose.Schema({
+//   sequenceNumber: { type: Number, required: true, unique: true },
+//   timestamps: true ,
+//   task: { type: String, required: true, maxlength : 20 },
+//   recipient: { type: String, required: true, maxlength: 40 },
+// })

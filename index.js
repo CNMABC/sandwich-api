8929c1006f8b73b061e314b16e5fe7ac2cdb4b99
Cecilia Nunn <cecilia.nunn@gmail.com>
2021-10-11 13:43:18 +0100

    successful get request

--- index.js
+++ index.js
@@ -5,18 +5,37 @@ const app = express()
 const port = 4000
 const dbURI = 'mongodb://localhost/sandwich-api'
 
+//definition of the schema
+const scheduleSchema = new mongoose.Schema({
+  sequenceNumber: { type: Number, required: true, unique: true },
+  task: { type: String, required: true, maxlength: 20 },
+  recipient: { type: String, required: true, maxlength: 20 }
+}, {
+  timestamps: true,
+})
+
+
+// Defining the model 
+const sandwichOrder = mongoose.model('sandwichOrder', scheduleSchema)
+
+// Logger -> to console log the incoming request method and the request url
+app.use((req, _res, next) => {
+  console.log(`🚨 Incoming request: METHOD: ${req.method}, URL: ${req.url}`)
+  next()
+})
+
+// Index route 
+app.get('/schedule', async (_req, res) => {
+  const schedule = await sandwichOrder.find()
+  console.log('schedule', schedule)
+  return res.status(200).json(schedule)
+})
+
 const startServer = async () => {
   try {
     // * connect to db
     await mongoose.connect(dbURI, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
     console.log('🚀 Database has connected successfully')
-
-    // * Logger -> to console log the incoming request method and the request url
-    app.use((req, _res, next) => {
-      console.log(`🚨 Incoming request: METHOD: ${req.method}, URL: ${req.url}`)
-      next()
-    })
-
     // * establish server connection
     app.listen(port, () => console.log(`🚀 Express is up and running on port ${port}`))
   } catch (err) {
@@ -28,4 +47,4 @@ const startServer = async () => {
 startServer()
 
 
-app.listen(port,() => console.log(`Express is up and running on port ${port}`))
+app.listen(port, () => console.log(`Express is up and running on port ${port}`))
